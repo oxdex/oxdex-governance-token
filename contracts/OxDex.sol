032e219c10cb7fd94b3b2f76c6f32b293f2e217a
islishude <islishude@gmail.com>
2021-03-12 23:52:15 +0800

    EIP2612 supports

--- contracts/OxDex.sol
+++ contracts/OxDex.sol
@@ -4,7 +4,71 @@ pragma solidity 0.5.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract OxDex is ERC20Detailed("OXDEX", "OX", 18), ERC20Capped(1e9 * 1e18) {
-    constructor() public {}
+contract OxDex is
+    ERC20,
+    ERC20Detailed("OXDEX", "OX", 18),
+    ERC20Capped(1e9 * 1e18)
+{
+    bytes32 public DOMAIN_SEPARATOR;
+    bytes32 public constant PERMIT_TYPEHASH =
+        keccak256(
+            "Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+        );
+
+    mapping(address => uint256) public nonces;
+
+    constructor() public {
+        uint256 chainId;
+        assembly {
+            chainId := chainid
+        }
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256(
+                    "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+                ),
+                keccak256(bytes(name())),
+                keccak256(bytes("1")),
+                chainId,
+                address(this)
+            )
+        );
+    }
+
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        require(deadline >= block.timestamp, "OX:permit: EXPIRED");
+        bytes32 digest =
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    DOMAIN_SEPARATOR,
+                    keccak256(
+                        abi.encode(
+                            PERMIT_TYPEHASH,
+                            owner,
+                            spender,
+                            value,
+                            nonces[owner]++,
+                            deadline
+                        )
+                    )
+                )
+            );
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(
+            recoveredAddress != address(0) && recoveredAddress == owner,
+            "OX:permit: INVALID_SIGNATURE"
+        );
+        _approve(owner, spender, value);
+    }
 }
